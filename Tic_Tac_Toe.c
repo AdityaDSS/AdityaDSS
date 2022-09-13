1b1736d7315e3d295909fc120d8a256463fedc53
AdityaDSS <89720147+AdityaDSS@users.noreply.github.com>
2022-09-13 21:48:22 +0530

    Update Tic_Tac_Toe.c
    Optimized the tic tac toe game.
    Now, the computer either wins or- if the user is intelligent - ends in a draw.

--- Tic_Tac_Toe.c
+++ Tic_Tac_Toe.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
 
 void main(){
+    //Initialization of the array
     char pos[9];
     pos[0] = '1';
     for (int i=1; i<9; i++)
-        pos[i] = pos[i-1] + 1;
+        pos[i] = pos[i-1] + 1; // filling the array
     int flag=1, i=0;
-    printf("The game looks like: \n");
+    printf("The game looks like:   (User-X & Comp. - O) \n");
     print_board(&pos);
+    printf("\n\n");
+    printf("Toss: (if the randomly generated integer number is even you play first) ");
+    time_t t;
+    srand((unsigned) time(&t));
+    int toss = rand();
+    printf("The number is %d.\n", toss);
+    int odd = (toss%2==0)?1:0;
+
     do{
         printf("\n");
         char mark;
         int c;
-        if (i%2==0){
+        if (odd){
+            here:
             mark='X';
-            printf("It's your turn Player1(enter a position in the grid): ");
+            printf("Your turn (enter a position in the grid): ");
+            odd=0;
         }
         else{
             mark='O';
-            printf("It's your turn Player2(enter a position in the grid): ");
+            printf("Now, it's my turn :) \n");
+            c=comp_turn(&pos);
+            char temp=c+48;
+            pos[c-1] = mark;
+            i++;
+            printf("The game looks like: (User-X & Comp. - O)\n");
+            print_board(&pos);
+            check_win(&pos, &flag);
+            if (!flag){
+                if (mark=='X')
+                    printf("\n^-^ You won Player1!\n");
+                else
+                    printf("\nI won!!!\n");
+                return;
+            }
+            odd=1;
+            continue;
         }
         scanf("%d", &c);
         char temp=c+48;
         if(c<1 || c>9 || pos[c-1]!= temp){
             printf("Enter a valid position.\n");
-            continue;
+            goto here;
         }
         pos[c-1] = mark;
         i++;
-        printf("The game looks like: \n");
+        printf("The game looks like: (User-X & Comp. - O)\n");
         print_board(&pos);
         check_win(&pos, &flag);
         if (!flag){
@@ -38,6 +68,7 @@ void main(){
                 printf("\n*-* You won Player2!\n");
             return;
         }
+        system("cls");
     }while(flag && i<9);
     if (i==9)
         printf("Match Drawn.Well played!");
@@ -60,4 +91,63 @@ check_win(char pos[], int *flag){
         *flag=0;
     else
         *flag=1;
+}
+
+int comp_turn(char pos[], int *flag){
+    int d[9], mark=1;
+    for(int i=0; i<9; i++){
+        if ( (pos[i]=='X') || (pos[i]=='O') ){
+            d[i]=-100;
+        }
+        else
+            d[i]=0;
+    }
+    for (int i=1; i<9; i++){
+        if (i==4)
+            continue;
+        if (d[i]!=0)
+            mark=0;
+    }
+    if (pos[4]=='X' && mark){
+        return 1;
+    }
+    for(int i=0; i<9; i++){
+        flag=1;
+        if(d[i]>=0){
+            pos[i]='X';
+            check_win(pos, &flag);
+            if(!flag){
+                pos[i]=i+ 49;
+                return (i+1);
+            }
+            pos[i]='O';
+            check_win(pos, &flag);
+            if (!flag){
+                pos[i] = i + 49;
+                return i+1;
+            }
+            pos[i]=i+49;
+        }
+    }
+    if (pos[4]=='O'){
+        if (pos[0]=='1' && pos[1]!='X' && pos[3]!='X')
+            return 1;
+        if (pos[2]=='3' && pos[1]!='X' && pos[5]!='X')
+            return 3;
+        if (pos[6]=='7' && pos[3]!='X' && pos[7]!='X')
+            return 7;
+        if (pos[8]=='9' && pos[7]!='X' && pos[5]!='X')
+            return 9;
+    }
+    if (pos[4]=='5')
+        return 5;
+    int max=0;
+    for (int i=0; i<9; i++){
+            if(d[i]>=max)
+                max=d[i];
+    }
+    for(int i=0; i<9; i++){
+        if(d[i]==max)
+            return i+1;
+    }
 }
